e1491333bfb6f66f5b911733caf892f2e897df79
ian-khan <66009288+ian-khan@users.noreply.github.com>
2025-12-04 21:26:53 +0800

    Modified SwinStep to encapsulate WindowAttention, Mlp, and SwinTransformerBlock of the official Swin Transformer inplementation.

--- ian/swin/swin_ian_verify.py
+++ ian/swin/swin_ian_verify.py
@@ -2,10 +2,19 @@ import torch
 from mmpose.models.backbones.rsn_ian_steps import SwinStep
 
 step = SwinStep(in_channels=64,
-                out_channels=64,
-                win_size=(2, 2))
-step_input = torch.randn(10 * 32 * 24, 4, 64)
+                input_size=(32, 24),
+                shift=(0, 0))
+step_input = torch.randn(10, 32, 24, 64)
 step_output = step.forward(step_input)
 print("\nSwin Step:")
 print(" Input  shape:", step_input.shape)
 print(" Output shape:", step_output.shape)
+
+shifted_step = SwinStep(in_channels=64,
+                        input_size=(32, 24),
+                        shift=(1, 1))
+shifted_step_input = torch.randn(10, 32, 24, 64)
+shifted_step_output = shifted_step.forward(shifted_step_input)
+print("\nShifted Swin Step:")
+print(" Input  shape:", shifted_step_input.shape)
+print(" Output shape:", shifted_step_output.shape)
--- mmpose/models/backbones/rsn_ian_steps.py
+++ mmpose/models/backbones/rsn_ian_steps.py
@@ -3,102 +3,158 @@ from torch import nn
 from typing import Optional
 
 class SwinStep(nn.Module):
+    """
+        B: batch size;
+        mh, mw: height and width of the map;
+        wh, ww: height and width of the window;
+        W: number of windows in the feature map;
+        H: number of heads;
+        T: number of tokens in a window, equal to wh * ww;
+        C: number of channels in the feature map;
+        hc: number of channels per head;
+    """
     def __init__(self,
                  in_channels: int,
-                 out_channels: int,
+                 input_size: tuple[int, int],
+                 shift: tuple[int, int],
                  head_channels: int=32,
+                 mlp_ratio: int=4,
                  win_size: tuple[int, int]=(2, 2),
                  qkv_bias: bool=True,
                  qk_scale: Optional[float]=None,
                  attn_drop: float=0.,
-                 proj_drop: float=0.,
+                 linear_drop: float=0.,
+                 act_layer: nn.Module=nn.GELU,
+                 norm_layer: nn.Module=nn.LayerNorm,
                  **kwargs):
         super().__init__()
 
-        assert out_channels % head_channels == 0, \
-            "out_channels must be divisible by head_channels"
+        assert in_channels % head_channels == 0, \
+            "in_channels must be divisible by head_channels"
+        assert input_size[0] % win_size[0] == 0 and input_size[1] % win_size[1] == 0,\
+            "input_size must be divisible by win_size"
+        assert 0 <= shift[0] < win_size[0] and 0 <= shift[1] < win_size[1], \
+            "shift must be in [0, win_size)"
 
         self.head_channels = head_channels
-        self.num_heads = out_channels // head_channels
+        self.num_heads = in_channels // head_channels
+        self.input_size = input_size
         self.win_size = win_size
         self.qk_scale = qk_scale or head_channels ** -0.5
+        self.shift = shift
+        self.do_shift = min(shift) > 0
 
-        self.bias_bucket = nn.Parameter(
+        self.norm1 = norm_layer(in_channels)
+
+        self.qkv = nn.Linear(in_channels, in_channels * 3, bias=qkv_bias)
+        self.softmax = nn.Softmax(dim=-1)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(in_channels, in_channels)
+        self.proj_drop = nn.Dropout(linear_drop)
+
+        self.norm2 = norm_layer(in_channels)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(in_channels, in_channels * mlp_ratio),
+            act_layer(),
+            nn.Dropout(linear_drop),
+            nn.Linear(in_channels * mlp_ratio, in_channels),
+            nn.Dropout(linear_drop),
+        )
+
+        # Relative position biases
+        self.bias_buckets = nn.Parameter(
             torch.zeros((2 * win_size[0] - 1) * (2 * win_size[1] - 1), self.num_heads))
         # bias bucket initialization
+
+        # Relative position biases index
         rel_pos_to_bias_bucket = \
             self.compute_rel_pos_to_bias_bucket(win_size[0], win_size[1])
         self.register_buffer('rel_pos_to_bias_bucket', rel_pos_to_bias_bucket)
 
-        self.qkv = nn.Linear(in_channels,
-                             out_channels * 3,
-                             bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.softmax = nn.Softmax(dim=-1)
-        self.proj = nn.Linear(out_channels,
-                              out_channels)
-        self.proj_drop = nn.Dropout(proj_drop)
+        # Attention mask
+        if self.do_shift:
+            attn_mask = self.compute_attn_mask(input_size, win_size, shift)
+            self.register_buffer('attn_mask', attn_mask)
 
-    def forward(self,
-                x: torch.Tensor,
-                mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
-        B: batch size;
-        W: number of windows in the feature map;
-        T: number of tokens in a window, equal to wh * ww;
-        C: number of channels in the feature map;
         Args:
             x: input tensor
-            mask: mask tensor of shape (W, T, T)
 
         Returns:
 
         """
-        BxW, T, C = x.shape
-        assert T == self.win_size[0] * self.win_size[1], "Number of tokens per window must be equal to wh * ww"
+        B, mh, mw, C = x.shape
+        assert (mh, mw) == self.input_size, "Input resolution is not correct"
+        wh, ww = self.win_size
+        W = (mh // wh) * (mw // ww)
+        T = wh * ww
 
-        qkv = self.qkv(x).reshape(BxW, T, 3, self.num_heads, self.head_channels)
-        qkv = qkv.permute(2, 0, 3, 1, 4)  # 3, BxW, nh, T, Ch
-        q, k, v = qkv[0], qkv[1], qkv[2]  # BxW, nh, T, Ch
+        shortcut = x
 
-        attn = (q @ k.transpose(-2, -1)) * self.qk_scale  # BxW, nh, T, T
+        x = self.norm1(x)
 
-        bias = self.bias_bucket[self.rel_pos_to_bias_bucket.view(-1)].view(T, T, self.num_heads)  # T, T, nh
-        bias = bias.permute(2, 0, 1).contiguous()  # nh, T, T
-        attn = attn + bias  # BxW, nh, T, T
+        if self.do_shift:
+            x = torch.roll(x, shifts=(-self.shift[0], -self.shift[1]), dims=(1, 2))
 
-        if mask is not None:
-            W = mask.shape[0]
-            attn = attn.view(BxW // W, W, self.num_heads, T, T)  # B, W, nh, T, T
-            attn = attn + mask.unsqueeze(1).unsqueeze(0)
-            attn = attn.view(BxW, self.num_heads, T, T)  # BxW, nh, T, T
+        x = self.window_partition(x, self.win_size)  # BxW, wh, ww, C
+
+        qkv = self.qkv(x).reshape(B*W, T, 3, self.num_heads, self.head_channels)
+        qkv = qkv.permute(2, 0, 3, 1, 4)  # 3, BxW, H, T, hc
+        q, k, v = qkv[0], qkv[1], qkv[2]  # BxW, H, T, hc
+
+        attn = (q @ k.transpose(-2, -1)) * self.qk_scale  # BxW, H, T, T
+
+        bias = self.bias_buckets[self.rel_pos_to_bias_bucket.view(-1)].view(T, T, self.num_heads)  # T, T, H
+        bias = bias.permute(2, 0, 1).contiguous()  # H, T, T
+        attn = attn + bias  # BxW, H, T, T
+
+        if self.do_shift:
+            attn = attn.view(B, W, self.num_heads, T, T)  # B, W, H, T, T
+            attn = attn + self.attn_mask
+            attn = attn.view(B*W, self.num_heads, T, T)  # BxW, nh, T, T
 
         attn = self.softmax(attn)
         attn = self.attn_drop(attn)
 
-        x = attn @ v
-        x = x.transpose(1, 2).reshape(BxW, T, C)
+        x = attn @ v  # BxW, H, T, hc
+        x = x.transpose(1, 2).reshape(B*W, T, C)  # BxW, T, C
 
         x = self.proj(x)
         x = self.proj_drop(x)
+
+        x = x.view(B*W, wh, ww, C)
+        x = self.window_stitch(x, self.input_size)  # B, mh, mw, C
+
+        if self.do_shift:
+            x = torch.roll(x, shifts=self.shift, dims=(1, 2))
+
+        x = x + shortcut
+
+        x = x.view(B, mh * mw, C)
+
+        x = x + self.mlp(x)
+
+        x= x.view(B, mh, mw, C)
         return x
 
     @staticmethod
-    def compute_rel_pos_to_bias_bucket(win_h: int,
-                                       win_w: int) -> torch.Tensor:
+    def compute_rel_pos_to_bias_bucket(wh: int,
+                                       ww: int) -> torch.Tensor:
         """
         Compute the mapping from relative position
         between query token and key token to bias bucket.
         Args:
-            win_h: height of the window
-            win_w: width of the window
+            wh: height of the window
+            ww: width of the window
 
         Returns: the mapping
 
         """
         # Create the coordinate sequence for tokens in a window
-        coord_h = torch.arange(win_h)
-        coord_w = torch.arange(win_w)
+        coord_h = torch.arange(wh)
+        coord_w = torch.arange(ww)
         coord_seq = torch.cartesian_prod(coord_h, coord_w)  # wh*ww, 2
 
         # Calculate pairwise relative positions between query and key tokens
@@ -107,10 +163,52 @@ class SwinStep(nn.Module):
 
         # Range of relative position is [-(wh-1), wh-1], [-(ww-1), ww-1]
         # Shift this range to non-negative [0, 2*(wh-1)], [0, 2*(ww-1)]
-        rel_pos[:, :, 0] += win_h - 1
-        rel_pos[:, :, 1] += win_w - 1
+        rel_pos[:, :, 0] += wh - 1
+        rel_pos[:, :, 1] += ww - 1
 
         # Map relative position to a number
-        rel_pos[:, :, 0] *= 2 * win_w - 1
+        rel_pos[:, :, 0] *= 2 * ww - 1
         rel_pos_to_bias_bucket = rel_pos.sum(-1)
         return rel_pos_to_bias_bucket
+
+    @staticmethod
+    def compute_attn_mask(input_size, win_size, shift) -> torch.Tensor:
+        region_chart = torch.zeros((1, input_size[0], input_size[1], 1))
+        h_slices = (slice(0, -win_size[0]),
+                    slice(-win_size[0], -shift[0]),
+                    slice(-shift[0], None))
+        w_slices = (slice(0, -win_size[1]),
+                    slice(-win_size[1], -shift[1]),
+                    slice(-shift[1], None))
+        region_index = 0
+        for h in h_slices:
+            for w in w_slices:
+                region_chart[0, h, w, 0] = region_index
+                region_index += 1
+
+        region_chart = SwinStep.window_partition(region_chart, win_size)  # W, wh, ww, 1
+        region_chart = region_chart.reshape(-1, win_size[0]*win_size[1])  # W, wh*ww or W, T
+
+        attn_mask = region_chart[:, :, None] - region_chart[:, None, :]  # W, T, T
+        attn_mask = attn_mask.masked_fill(attn_mask == 0, 0.0).masked_fill(attn_mask != 0, -100.0)
+        attn_mask = attn_mask.unsqueeze(1).unsqueeze(0)  # 1, W, 1, T, T
+        return attn_mask
+
+    @staticmethod
+    def window_partition(x: torch.Tensor, win_size) -> torch.Tensor:
+        B, mh, mw, C = x.shape
+        wh, ww = win_size
+        x = x.view(B, mh // wh, wh, mw // ww, ww, C)
+        x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, wh, ww, C)  # BxW, wh, ww, C
+        return x
+
+    @staticmethod
+    def window_stitch(x: torch.Tensor, map_size) -> torch.Tensor:
+        BxW, wh, ww, C = x.shape
+        mh, mw = map_size
+        W = (mh // wh) * (mw // ww)
+        B = BxW // W
+        x = x.view(B, mh // wh, mw // ww, wh, ww, C)
+        x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, mh, mw, C)
+        return x
+
