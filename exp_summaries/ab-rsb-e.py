e52b52b1f2d3ec841e2b0e278fb24b3e88384b76
Ian Khan <66009288+ian-khan@users.noreply.github.com>
2026-04-14 15:35:41 +0800

    Modified the Config ian_ab-rsb-e-5_td-hm_rsn50_3xb128-200e_coco-256x192.py.

--- exp_summaries/ab-rsb-e.py
+++ exp_summaries/ab-rsb-e.py
@@ -6,26 +6,30 @@ experiments = [
                              "ian_ab-blk-att-2_td-hm_rsn50-c_3xb128-200e_coco-256x192"),
                exp_times="20260209_133443",
                label="RSB-C Exp 2"),
+    # Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
+    #                          "ian_ab-rsb-e-0_td-hm_rsn50_3xb128-200e_coco-256x192"),
+    #            exp_times="20260407_152714",
+    #            label="RSB-E Exp 0"),
+    # Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
+    #                          "ian_ab-rsb-e-1_td-hm_rsn50_3xb128-200e_coco-256x192"),
+    #            exp_times=["20260408_211759", "20260409_174028"],
+    #            label="RSB-E Exp 1"),
+    # Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
+    #                          "ian_ab-rsb-e-2_td-hm_rsn50_3xb128-200e_coco-256x192"),
+    #            exp_times=["20260410_193705", "20260411_005043"],
+    #            label="RSB-E Exp 2"),
+    # Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
+    #                          "ian_ab-rsb-e-3_td-hm_rsn50_3xb128-200e_coco-256x192"),
+    #            exp_times=["20260411_223926"],
+    #            label="RSB-E Exp 3"),
+    # Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
+    #                          "ian_ab-rsb-e-4_td-hm_rsn50_3xb128-200e_coco-256x192"),
+    #            exp_times=["20260413_082025"],
+    #            label="RSB-E Exp 4"),
     Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
-                             "ian_ab-rsb-e-0_td-hm_rsn50_3xb128-200e_coco-256x192"),
-               exp_times="20260407_152714",
-               label="RSB-E Exp 0"),
-    Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
-                             "ian_ab-rsb-e-1_td-hm_rsn50_3xb128-200e_coco-256x192"),
-               exp_times=["20260408_211759", "20260409_174028"],
-               label="RSB-E Exp 1"),
-    Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
-                             "ian_ab-rsb-e-2_td-hm_rsn50_3xb128-200e_coco-256x192"),
-               exp_times=["20260410_193705", "20260411_005043"],
-               label="RSB-E Exp 2"),
-    Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
-                             "ian_ab-rsb-e-3_td-hm_rsn50_3xb128-200e_coco-256x192"),
-               exp_times=["20260411_223926"],
-               label="RSB-E Exp 3"),
-    Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
-                             "ian_ab-rsb-e-4_td-hm_rsn50_3xb128-200e_coco-256x192"),
-               exp_times=["20260413_082025"],
-               label="RSB-E Exp 4"),
+                             "ian_ab-rsb-e-5_td-hm_rsn50_3xb128-200e_coco-256x192"),
+               exp_times=["20260413_224229"],
+               label="RSB-E Exp 5"),
 ]
 
 Experiment.draw_iter_lr_curve(experiments, output="show")
