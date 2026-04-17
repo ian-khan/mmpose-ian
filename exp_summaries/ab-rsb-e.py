64ed7f19eefffd9b72298d59ace10a9067f2a795
Ian Khan <66009288+ian-khan@users.noreply.github.com>
2026-04-17 18:30:02 +0800

    Modified the Config ian_ab-rsb-e-7_td-hm_rsn50_3xb128-200e_coco-256x192.py.

--- exp_summaries/ab-rsb-e.py
+++ exp_summaries/ab-rsb-e.py
@@ -34,6 +34,10 @@ experiments = [
                              "ian_ab-rsb-e-6_td-hm_rsn50_3xb128-200e_coco-256x192"),
                exp_times=["20260415_090957", "20260416_160013"],
                label="RSB-E Exp 6"),
+    Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
+                             "ian_ab-rsb-e-7_td-hm_rsn50_3xb128-200e_coco-256x192"),
+               exp_times=["20260416_201342"],
+               label="RSB-E Exp 7"),
 ]
 
 Experiment.draw_iter_lr_curve(experiments, output="show")
