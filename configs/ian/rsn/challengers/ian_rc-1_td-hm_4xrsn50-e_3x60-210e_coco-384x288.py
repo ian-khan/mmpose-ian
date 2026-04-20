578be45a98a38dac1ddd1c5015b159a0a785eea2
Ian Khan <66009288+ian-khan@users.noreply.github.com>
2026-04-20 18:39:15 +0800

    Modified the ian_rc-1_td-hm_4xrsn50-e_3x64-210e_coco-384x288.py Config. The batch size is tuned down to 40 to fit in VRAM.

--- configs/ian/rsn/challengers/ian_rc-1_td-hm_4xrsn50-e_3x60-210e_coco-384x288.py
+++ configs/ian/rsn/challengers/ian_rc-1_td-hm_4xrsn50-e_3x60-210e_coco-384x288.py
@@ -146,7 +146,7 @@ val_pipeline = [
 
 # data loaders
 train_dataloader = dict(
-    batch_size=64,
+    batch_size=60,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
@@ -159,7 +159,7 @@ train_dataloader = dict(
         pipeline=train_pipeline,
     ))
 val_dataloader = dict(
-    batch_size=64,
+    batch_size=60,
     num_workers=4,
     persistent_workers=True,
     drop_last=False,
--- exp_summaries/rc.py
+++ exp_summaries/rc.py
@@ -6,17 +6,21 @@ exps = [
                              "ian_td-hm_4xrsn-alter-c_3xb64-210e_coco-384x288"),
                exp_times="20260112_154914",
                label="Exp 210e"),
+    # Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
+    #                          "ian_rc_td-hm_4xrsn50-c_3xb64-360e_coco-384x288"),
+    #            exp_times=["20260215_165825", "20260219_123923",
+    #                       "20260226_131658", "20260305_173510",
+    #                       "20260306_222237", "20260308_001251",
+    #                       "20260311_221326", "20260312_174947"],
+    #            label="Exp -1"),
+    # Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
+    #                          "ian_rc-0_td-hm_4xrsn50-c_3x64-360e_coco-384x288"),
+    #            exp_times=["20260322_115737", "20260329_003404"],
+    #            label="Exp 0"),
     Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
-                             "ian_rc_td-hm_4xrsn50-c_3xb64-360e_coco-384x288"),
-               exp_times=["20260215_165825", "20260219_123923",
-                          "20260226_131658", "20260305_173510",
-                          "20260306_222237", "20260308_001251",
-                          "20260311_221326", "20260312_174947"],
-               label="Exp -1"),
-    Experiment(work_dir=Path("/data/ian/remote_projects/mmpose/work_dirs/"
-                             "ian_rc-0_td-hm_4xrsn50-c_3x64-360e_coco-384x288"),
-               exp_times=["20260322_115737", "20260329_003404"],
-               label="Exp 0"),
+                             "ian_rc-1_td-hm_4xrsn50-e_3x60-210e_coco-384x288"),
+               exp_times=["20260419_151132", "20260420_144400"],
+               label="Exp 1"),
 ]
 
 Experiment.draw_iter_lr_curve(exps, output="show")
